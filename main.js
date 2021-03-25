2e4ec079518926378749afc1819f81de84a348f4
clannis <dodds1990@gmail.com>
2021-03-25 14:36:06 -0500

    Add logic to shrink file and open new window at location of resized file

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const { app, BrowserWindow, Menu, globalShortcut, ipcMain } = require('electron')
+const path = require('path')
+const os = require('os')
+const { app, BrowserWindow, Menu, globalShortcut, ipcMain, shell } = require('electron')
+const imagemin = require('imagemin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
+const slash = require('slash')
 
 // Set env
 process.env.NODE_ENV = 'development'
@@ -93,8 +99,29 @@ const menu = [
 ]
 
 ipcMain.on('image:minimize', (e, options) => {
-    console.log(options)
+    options.destination = path.join(os.homedir(), 'imageshrink')
+    shrinkImage(options)
+    
 })
+
+async function shrinkImage({imgPath, quality, destination}) {
+    try {
+        const pngQuality = quality/100
+        const files = await imagemin([slash(imgPath)], {
+            destination,
+            plugins: [
+                imageminMozjpeg({ quality }),
+                imageminPngquant({
+                    quality: [pngQuality, pngQuality]
+                })
+            ]
+        })
+        
+        shell.openPath(destination)
+    } catch (err) {
+        console.log(err)
+    }
+}
 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
