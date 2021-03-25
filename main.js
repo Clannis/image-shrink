f6611098f3b54bba6c92334467c5951c6271d058
Clannis <dodds1990@gmail.com>
2021-03-24 22:40:57 -0500

    Set env and flag variables. Set window to resizable only in development. Add default mac app functionality on window closed

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const { app, BrowserWindow } = require('electron')
 
+// Set env
+process.env.NODE_ENV = 'development'
+
+// Set flags
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+const isMac = process.platform === 'darwin' ? true : false
+
 let mainWindow
 
 function createMainWindow () {
@@ -7,10 +14,25 @@ function createMainWindow () {
         title: 'Image Shrink',
         width: 500,
         height: 600,
-        icon: `${__dirname}/assets/icons/Icon_256x256.png`
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+        resizable: isDev
     })
 
     mainWindow.loadFile('./app/index.html')
 }
 
 app.on('ready', createMainWindow)
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow()
+    }
+})
+
+// Keeps app running when all windows closed
+//
+// app.on('window-all-closed', () => {
+//     if (!isMac) {
+//         app.quit()
+//     }
+// })
