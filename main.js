5fbcab0428e559ec6300b688828b62399a00429d
clannis <dodds1990@gmail.com>
2021-03-25 13:08:08 -0500

    Add functionality to create about window on click from new menu item.

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const isDev = process.env.NODE_ENV !== 'production' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
 
 let mainWindow
+let aboutWindow
 
 function createMainWindow () {
     mainWindow = new BrowserWindow({
@@ -22,14 +23,24 @@ function createMainWindow () {
     mainWindow.loadFile('./app/index.html')
 }
 
+function createAboutWindow () {
+    aboutWindow = new BrowserWindow({
+        title: 'About Image Shrink',
+        width: 300,
+        height: 300,
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+        resizable: false,
+        backgroundColor: 'white'
+    })
+
+    aboutWindow.loadFile('./app/about.html')
+}
+
 app.on('ready', () => {
     createMainWindow()
 
     const mainMenu = Menu.buildFromTemplate(menu)
     Menu.setApplicationMenu(mainMenu)
-
-    globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
-    globalShortcut.register(isMac ? 'Cmd+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
 
     mainWindow.on('closed', () => mainWindow = null)
 })
@@ -37,9 +48,25 @@ app.on('ready', () => {
 const menu = [
     ...(isMac ? [
         {
-            role: 'appMenu'
+            label: app.name,
+            submenu: [
+                { 
+                    label: 'About',
+                    click: createAboutWindow
+                }
+            ]
         }
-    ] : []),
+    ] : [
+        {
+            label: "Help",
+            submenu: [
+                { 
+                    label: 'About',
+                    click: createAboutWindow
+                }
+            ]
+        }
+    ]),
     {
         role: 'fileMenu'
     },
